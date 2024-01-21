a624af6a08b8a5cac2dc9b8b839fcc1f85dbd4ca
Artyom <pragmatist1618@gmail.com>
2024-01-21 13:00:10 +0300

    first push

---
+++ main.py
@@ -0,0 +1,15 @@
+import torch
+import numpy as np
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    shape = (2, 3,)
+    rand_tensor = torch.rand(shape)
+    ones_tensor = torch.ones(shape)
+    zeros_tensor = torch.zeros(shape)
+
+    print(f"Random Tensor: \n {rand_tensor} \n")
+    print(f"Ones Tensor: \n {ones_tensor} \n")
+    print(f"Zeros Tensor: \n {zeros_tensor}")
+
